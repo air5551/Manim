3432ad4a994f0b23768fd5be8542ae903514f156
air5551 <zacupham102@gmail.com>
2024-10-19 12:58:20 -0400

    edited the Align class and and added the Rect class

--- Circle.py
+++ Circle.py
@@ -1,10 +1,11 @@
-from manim import * 
+from manim import * #type: ignore
 from pathops import Direction
 # Global Variables
 tri = Triangle()
 circle = Circle()
 square = Square()
 cone = Cone()
+rectangle = Rectangle()
 class CreateCircle(Scene):
     def construct(self):
         circle = Circle()
@@ -45,22 +46,31 @@ class New(Scene):
         self.play(Create(tri3))
         self.play(square.animate.rotate(PI/4)) #type: ignore
         self.play(tri.animate.rotate_about_origin(PI/2)) #type: ignore
-        self.play(tri2.animate.rotate_about_origin(PI/5)) #type: ignore
+        self.play(tri2.animate.rotate_about_origin(PI/6)) #type: ignore
         self.play(tri3.animate.rotate_about_origin(PI/5)) #type: ignore
 class Align(Scene):
     def construct(self):
         tri2 = Triangle()
         tri3 = Triangle()
+        tri4 = Triangle()
         tri.next_to(square,direction=RIGHT, buff=0.5)
-        tri2.next_to(square,direction=LEFT, buff=0.5)
+        tri2.next_to(square,direction=UP, buff=0.7)
         tri3.next_to(square, direction=DOWN, buff=0.5)
+        tri4.next_to(square, direction=LEFT, buff=0.5)
         square.set_fill(color=GREEN, opacity=0.4)
         self.play(Create(square))
         self.play(Create(tri))
         self.play(Create(tri2))
         self.play(Create(tri3))
-        self.play(square.animate.rotate(PI/4))
-        self.play(tri3.animate.rotate(PI/4))
-        self.play(tri2.animate.rotate(PI/4))
-        self.play(tri.animate.rotate(PI/4))
-        
+        self.play(Create(tri4))
+        self.play(square.animate.rotate(PI/4)) #type: ignore
+        self.play(tri4.animate.rotate(-1.570796327)) #type: ignore
+        self.play(tri2.animate.rotate(PI)) #type: ignore
+        self.play(tri.animate.rotate(1.5708)) #type: ignore
+
+
+class Rect(Scene):
+    def construct(self):
+        rectangle.set_fill(color=GREEN, opacity=0.5)
+        self.play(Create(rectangle))
+        self.play(rectangle.animate.rotate(PI/4)) #type: ignore
